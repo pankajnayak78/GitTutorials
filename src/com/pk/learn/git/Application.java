351e4d78fea4846ddfb02b2f92ee45e67cd77109
pankajnayak78 <pankajnayak20@gmail.com>
2017-12-03 21:55:09 +0530

    Add src folder for project

---
+++ src/com/pk/learn/git/Application.java
@@ -0,0 +1,15 @@
+package com.pk.learn.git;
+
+/**
+ * @author PK
+ * @since Dec 3, 2017 9:52:34 PM
+ */
+public class Application {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello...Welcome to git.");
+	}
+}
