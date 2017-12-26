073eadc1f53d028d9101b126be78595423c8641a
pankajnayak78 <pankajnayak20@gmail.com>
2017-12-26 22:49:43 +0530

    add tag v1.0

--- src/com/pk/learn/git/Application.java
+++ src/com/pk/learn/git/Application.java
@@ -8,6 +8,8 @@ public class Application {
 
 	public static void main(String[] args) {
 		System.out.println("Hi.");
+		System.out.println("Hi...Adding tag...");
 		System.out.println("Hello...Welcome to git.");
+		System.out.println("Hello tag v1.0^");
 	}
 }
